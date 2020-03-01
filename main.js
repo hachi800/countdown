865112b5a6d50aeef76d49c15c34f7ef4a037d1a
hachi800 <hachi800@users.noreply.github.com>
2020-03-01 14:41:05 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+window.onload = firstscript;
+setInterval('time()',1000);
+
+function firstscript(){
+    time();
+}
+
+function time(){
+    var now = new Date();
+    var point = new Date(2020,2,20,00,00);
+    var limit = new Date();
+    var limitSec = (Math.floor(point.getTime() / 1000))-(Math.floor(now.getTime() / 1000));
+    var h = Math.floor(limitSec / 3600);
+    var m = Math.floor((limitSec % 3600) / 60);
+    var s = ((limitSec % 3600) % 60);
+
+    document.getElementById("limit").innerHTML = h + ":" + ('00'+m).slice(-2) + ":" + ('00'+s).slice(-2);
+}   
+
+function twitterMyAccount(){
+    window.location.href = "https://twitter.com/HACHIGUILE";
+}
